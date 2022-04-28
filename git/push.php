3e1c2f492fbb9569bc1f95d93529e9a9d4ac2e9f
NPEX42 <NPEX42@gmail.com>
2022-04-28 19:04:00 +0100

    Updated main.css & push.php

--- git/push.php
+++ git/push.php
@@ -2,7 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-shell_exec( 'cd /var/www/npex42.dev/html/website && git pull --force' );
+shell_exec( 'cd /var/www/npex42.dev/html/website && git reset --hard HEAD && git pull --force' );
 
 
 ?>
