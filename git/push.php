0c71b5f4ca89df9dbe0cc041805c75d7f362f181
George Venn <npex42@gmail.com>
2022-04-30 16:08:03 +0000

    Updated push.php

--- git/push.php
+++ git/push.php
@@ -2,7 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-shell_exec( 'cd /var/www/npex42.dev/html/website && git reset --hard HEAD && git pull --force' );
-
+$result = shell_exec( 'cd /var/www/npex42.dev/html/website && git checkout prod && git pull --force' );
+echo $result
 
 ?>
